fb3a85e93e8903a85818082b712a6b01183a8c3d
RajaBijoriya <rajabijoriya875@gmail.com>
2026-01-20 14:55:35 +0530

    url for backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = ' https://hospital-backend-9h3a.onrender.com';
 
 export const authAPI = {
     register: (data) => axios.post(`${API_BASE_URL}/auth/register`, data),
